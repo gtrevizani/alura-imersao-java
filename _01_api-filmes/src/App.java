21dcbab4dcda4d9a544bf14855c16aa64a06e677
gtrevizani <guilhermetrevz@outlook.com>
2023-03-28 15:48:26 -0300

    Convertendo imagens para figurinhas - 28/03

--- _01_api-filmes/src/App.java
+++ _01_api-filmes/src/App.java
@@ -5,6 +5,8 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
+import java.io.InputStream;
+import java.net.URL;
 
 public class App {
     public static void main(String[] args) throws Exception { 
@@ -20,37 +22,24 @@ public class App {
 
         // extrair só os dados que interessam (titulo, poster, classificação)
         JsonParser parser = new JsonParser();
-        List<Map<String,String>> filmes = parser.parse(body);
+        List<Map<String,String>> listaDeFilmes = parser.parse(body);
         
         //exibir e manipular os dados
-        for(Map<String,String> filme : filmes) {
+        var geradora = new GeradoraDeFigurinhas();
+        for(Map<String,String> filme : listaDeFilmes) {
 
-            System.out.println("\u001b[1m \u001b[34m Title: \u001b[m" + filme.get("title"));
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
             
-            System.out.println("\u001b[1m \u001b[34m Poster: \u001b[m" + filme.get("image"));
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo.replace(":", "-") + ".png";
 
-            int stars = (int) Double.parseDouble(filme.get("imDbRating"));
-            String color = changeColor(stars);
+            
+            geradora.cria(inputStream, nomeArquivo);
 
-            System.out.print("\u001b[1m " +color+ "Rating: " + filme.get("imDbRating")+" (");
-            for(int i=0; i<stars ;i++) {
-                System.out.print("\u2B50");
-            }
-            System.out.println(")\u001b[m\n");
+            System.out.println(filme.get("title"));
+            System.out.println();
         }
     }
 
-    public static String changeColor(int number) {
-        switch(number) {
-            case 9:
-                return "\u001b[42m";
-            case 8:
-                return "\u001b[43m";
-            case 7:
-                return "\u001b[41m";
-            default:
-                return "\u001b[47m";
-        }
-        
-    }
 } 
